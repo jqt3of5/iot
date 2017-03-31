b623a419401f1212b9434efd3ce6ac8e61414448
unknown <jqt3of5@gmail.com>
2017-03-30 21:25:02 -0600

    added events

--- photon/temp/main.cpp
+++ photon/temp/main.cpp
@@ -12,18 +12,10 @@
 #define C 0.0000001323120683
 #define SAMPLES 10
 
-double T0 = 0;
-double T1 = 0;
-double T2 = 0;
-
-double state_of_charge = 0;
+char status[100] = {0};
 void setup()
 {
-  Particle.variable("T0",T0);
-  Particle.variable("T1",T1);
-  Particle.variable("T2",T2);
-
-  Particle.variable("charge", state_of_charge);
+  Particle.variable("status", status);
 
   lipo.begin();
   lipo.quickStart();
@@ -31,7 +23,7 @@ void setup()
 
 void loop()
 {
-  state_of_charge = lipo.getSOC();
+  double state_of_charge = lipo.getSOC();
 
   double temp_a0 = 0, temp_a1 = 0, temp_a2 = 0;
   for (int i = 0; i < SAMPLES; ++i)
@@ -39,7 +31,7 @@ void loop()
     temp_a0 += analogRead(A0);
     temp_a1 += analogRead(A1);
     temp_a2 += analogRead(A2);
-    delay(500);
+    delay(100);
   }
   //Average the readings.
   temp_a0 = temp_a0/SAMPLES;
@@ -52,12 +44,28 @@ void loop()
   double avg_R2 = temp_a2*R1/(MAX_A - temp_a2);;
 
   //Convert the resistance into a tempurature using the precomputed Stienhart-Hart coefficients.
-  T0 = 1/(A + B*log(avg_R0) + C*log(avg_R0)*log(avg_R0)*log(avg_R0)) - 273.15;
-  T1 = 1/(A + B*log(avg_R1) + C*log(avg_R1)*log(avg_R1)*log(avg_R1)) - 273.15;
-  T2 = 1/(A + B*log(avg_R2) + C*log(avg_R2)*log(avg_R2)*log(avg_R2)) - 273.15;
+  double T0 = 1/(A + B*log(avg_R0) + C*log(avg_R0)*log(avg_R0)*log(avg_R0)) - 273.15;
+  double T1 = 1/(A + B*log(avg_R1) + C*log(avg_R1)*log(avg_R1)*log(avg_R1)) - 273.15;
+  double T2 = 1/(A + B*log(avg_R2) + C*log(avg_R2)*log(avg_R2)*log(avg_R2)) - 273.15;
 
-  char data[100] = {0};
+  sprintf(status, "{\"T0\":\"%f\",\"T1\":\"%f\",\"T2\":\"%f\",\"charge\":\"%f\"}", T0, T1, T2, state_of_charge);
+
+  static bool chargeEventOnce = false;
+  if (state_of_charge < 10)
+  {
+    if (!chargeEventOnce)
+    {
+      Particle.publish("low_charge", state_of_charge);
+      chargeEventOnce = true;
+    }
+  }
+  else
+  {
+    chargeEventOnce = false;
+  }
+  
+/*  char data[100] = {0};
   sprintf(data, "{\"probeA\":%f, \"probeB\":%f, \"probeC\":%f}", T0, T1, T2);
   Particle.publish("temp", data, 0, PRIVATE);
-  delay(10000);
+  delay(10000);*/
 }
